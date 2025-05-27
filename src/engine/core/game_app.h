06c96b56fa5887260bb07bf6f33430e3bfff5e0a
Ziyu <theorhythm@gmail.com>
2025-05-27 17:23:07 +0800

    完成场景与场景管理

--- src/engine/core/game_app.h
+++ src/engine/core/game_app.h
@@ -18,6 +18,10 @@ namespace engine::input {
 class InputManager;
 }
 
+namespace engine::scene {
+class SceneManager;
+}
+
 namespace engine::core {        // 命名空间的最佳实践：与文件路径一致
 class Time;
 class Config;
@@ -40,6 +44,7 @@ private:
     std::unique_ptr<engine::core::Config> config_;
     std::unique_ptr<engine::input::InputManager> input_manager_;
     std::unique_ptr<engine::core::Context> context_;
+    std::unique_ptr<engine::scene::SceneManager> scene_manager_;
 
 public:
     GameApp();
@@ -72,13 +77,7 @@ private:
     [[nodiscard]] bool initCamera();
     [[nodiscard]] bool initInputManager();
     [[nodiscard]] bool initContext();
-
-    // 测试用函数
-    void testResourceManager();
-    void testRenderer();
-    void testCamera();
-    void testInputManager();
-    void testGameObject();
+    [[nodiscard]] bool initSceneManager();
 };
 
 } // namespace engine::core
