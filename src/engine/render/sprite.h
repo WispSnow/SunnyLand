b607b9e1dbbb00110d4a04f5aeb701ad98d1b7bb
Ziyu <theorhythm@gmail.com>
2025-05-30 16:23:12 +0800

    完成关卡加载器-瓦片层

--- src/engine/render/sprite.h
+++ src/engine/render/sprite.h
@@ -20,6 +20,11 @@ private:
 
 public:
     /**
+     * @brief 默认构造函数（创建一个空的/无效的精灵）
+     */
+    Sprite() = default;
+    
+    /**
      * @brief 构造一个精灵
      *
      * @param texture_id 纹理资源的标识符。不应为空。
